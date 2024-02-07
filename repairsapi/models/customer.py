377197aed8683b021a4ab5b9fc86dca3a522b3d4
Frankie Bruno <36001544+FrankieBruno@users.noreply.github.com>
2024-02-06 20:26:45 -0600

    new

--- repairsapi/models/customer.py
+++ repairsapi/models/customer.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-
     # Relationship to the built-in User model which has name and email
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Additional address field to capture from the client
