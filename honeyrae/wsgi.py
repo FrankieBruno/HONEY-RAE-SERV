b8057b2b7eb9cadad8f6f10fca3ea2fddcaed2d7
Frankie Bruno <36001544+FrankieBruno@users.noreply.github.com>
2024-02-07 17:17:39 -0600

    new

--- honeyrae/wsgi.py
+++ honeyrae/wsgi.py
@@ -8,9 +8,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
-import get_wsgi_application
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'honeyrae.settings')
