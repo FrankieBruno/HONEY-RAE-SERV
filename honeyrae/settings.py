aafe6ae960c37565a65840c2563344f518bb62e8
Frankie Bruno <36001544+FrankieBruno@users.noreply.github.com>
2024-02-07 17:29:46 -0600

    new

--- honeyrae/settings.py
+++ honeyrae/settings.py
@@ -144,9 +144,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 
 # Default primary key field type
